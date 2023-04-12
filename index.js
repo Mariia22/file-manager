ce74509bf4792aa9fe18b35d75ccdd3d429c9f98
Maria22 <m.ushakova22@gmail.com>
2023-04-12 22:09:04 +0200

    feat: add readline to process

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log("Hi")
+import * as readline from 'readline'
+
+let username = process.argv ? process.argv : "John Doe"
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: 'OHAI> '
+});
+
+rl.prompt()
+
+const commands = {
+  add: function () {
+    console.log("Add")
+  },
+  bye: function () { rl.close() }
+}
+
+rl.on('line', (input) => {
+  try {
+    if (input.trim() in commands) {
+      commands[input]()
+    }
+  } catch {
+    throw new Error("Error")
+  }
+  rl.prompt()
+})
+
+rl.on('SIGINT', () => {
+  process.exit();
+})
+
+rl.on('close', () => {
+  process.exit();
+})
