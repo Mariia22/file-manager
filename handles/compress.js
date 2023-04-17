5d5050cbb974e1bac8a3bd86848c6fbfbde36f37
Maria22 <m.ushakova22@gmail.com>
2023-04-17 16:20:01 +0200

    feat: add compress and decompress func

--- handles/compress.js
+++ handles/compress.js
@@ -1,3 +1,19 @@
-export const compressFile = async () => {
+import { createReadStream, createWriteStream } from 'fs'
+import { createBrotliCompress } from 'zlib'
+import { pipeline } from 'stream/promises'
+import { resolve, parse } from 'path'
 
+export const compressFile = async (paths) => {
+  if (!paths) {
+    console.log("Command is not recognized. Add path to source file and destination folder")
+  }
+  const [source, destination] = paths.split(',')
+  const sourceFile = resolve(source)
+  const { base } = parse(sourceFile)
+  const fileName = `${base}.br`
+  const destinationPath = resolve(destination, fileName)
+  const readStream = createReadStream(sourceFile)
+  const writableStream = createWriteStream(destinationPath)
+  const brotlitCompress = createBrotliCompress()
+  await pipeline(readStream, brotlitCompress, writableStream)
 }
--- handles/decompress.js
+++ handles/decompress.js
@@ -1,3 +1,19 @@
-export const decompressFile = async () => {
+import { createReadStream, createWriteStream } from 'fs'
+import { createBrotliDecompress } from 'zlib'
+import { pipeline } from 'stream/promises'
+import { resolve, parse } from 'path'
 
+export const decompressFile = async (paths) => {
+  if (!paths) {
+    console.log("Command is not recognized. Add path to source file and destination folder")
+  }
+  const [source, destination] = paths.split(',')
+  const sourceFile = resolve(source)
+  const { name } = parse(sourceFile)
+  const destinationPath = resolve(destination, name)
+  console.log(destinationPath)
+  const readStream = createReadStream(sourceFile)
+  const writableStream = createWriteStream(destinationPath)
+  const brotlitDecompress = createBrotliDecompress()
+  await pipeline(readStream, brotlitDecompress, writableStream)
 }
